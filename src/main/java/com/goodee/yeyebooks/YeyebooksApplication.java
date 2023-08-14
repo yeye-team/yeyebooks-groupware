d99d414598f8418f36af14f4e819b01546ad4d9e
seokhyun03 <tjrgus0303@gmail.com>
2023-08-14 17:56:44 +0900

    project setting

---
+++ src/main/java/com/goodee/yeyebooks/YeyebooksApplication.java
@@ -0,0 +1,13 @@
+package com.goodee.yeyebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeyebooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeyebooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodee/yeyebooks/YeyebooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodee.yeyebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeyebooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
