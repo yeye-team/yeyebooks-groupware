0d179f17346a76ad9cb00082b88b7f58b79ab5da
seokhyun03 <tjrgus0303@gmail.com>
2023-08-16 17:38:07 +0900

    정석현 / 부서 조회, 추가, 수정, 삭제 sql 맵핑

---
+++ src/main/java/com/goodee/yeyebooks/mapper/DeptMapper.java
@@ -0,0 +1,17 @@
+package com.goodee.yeyebooks.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DeptMapper {
+List<Map<String, Object>> selectDeptList();
+	
+	int insertDept(Map<String, Object> map);
+	
+	int deleteDept(Map<String, Object> map);
+	
+	int updateDept(Map<String, Object> map);
+}
