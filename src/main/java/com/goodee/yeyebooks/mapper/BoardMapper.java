f18f16e11c587bc2cbf923798a2ea23b71ce1ca4
Sanlee27 <010121qqq@naver.com>
2023-08-17 17:37:08 +0900

    이강산 / BoardMapper_게시판 별 게시물 리스트 조회

--- src/main/java/com/goodee/yeyebooks/mapper/BoardMapper.java
+++ src/main/java/com/goodee/yeyebooks/mapper/BoardMapper.java
@@ -6,17 +6,20 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.goodee.yeyebooks.vo.Board;
+import com.goodee.yeyebooks.vo.User;
 
 @Mapper
 public interface BoardMapper {
-	// 공지사항 리스트 조회
-	List<Board> selectNotice(Map<String, Object> map);
+	// 게시판 별 게시물 리스트 조회
+	List<Board> selectBoard(Map<String, Object> map);
+	
+	// 게시판 별 게시물 전체 개수
+	int selectBoardCount(String boardCatCd);
+	
+	// 부서 게시판 조회를 위한 사용자 부서 코드 조회
+	String selectUserDept (String userId);
 	
 	// 공지사항 상세 조회
 	Board selectnNoticeOne(int boardNo);
-	
-	
-	// 공지사항 전체 개수
-	int selectNoticeCount();
 	
 }
