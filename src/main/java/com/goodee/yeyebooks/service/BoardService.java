7dff19312988adf127417b2992679d2eedfd07d3
Sanlee27 <010121qqq@naver.com>
2023-08-17 17:38:45 +0900

    이강산 / BoardService_게시판 별 게시물 리스트 조회 완료
    - userId : session에서 받아온 값으로 변경 필요

--- src/main/java/com/goodee/yeyebooks/service/BoardService.java
+++ src/main/java/com/goodee/yeyebooks/service/BoardService.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.websocket.Session;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.goodee.yeyebooks.mapper.BoardFileMapper;
 import com.goodee.yeyebooks.mapper.BoardMapper;
@@ -20,28 +23,41 @@ import lombok.extern.slf4j.Slf4j;
 public class BoardService {
 	@Autowired
 	private BoardMapper boardMapper;
-	@Autowired
-	private BoardFileMapper boardfileMapper;
+	/*
+	 * @Autowired private BoardFileMapper boardfileMapper;
+	 */
 	
-	// 공지사항 리스트 조회
-	public Map<String, Object> selectNoticeList(int currentPage, int rowPerPage) {
+	// 게시판 별 게시물 리스트 조회
+	public Map<String, Object> selectBoardList(int currentPage, int rowPerPage, String boardCatCd) {
 		// 첫행
 		int beginRow = (currentPage-1) * rowPerPage;
 		
+		// ********통합테스트시 session으로 변경하기*********
+		String userId = "m23081702";
+		String deptCd = boardMapper.selectUserDept(userId);
+		log.debug("\u001B[41m"+ deptCd + "< deptCd" + "\u001B[0m");	
+		
+		// 부서게시판 클릭시 부서 value값과 동일하다면 사용자의 부서코드를 저장
+		if(boardCatCd.equals("dept")) {
+			boardCatCd = deptCd;
+			System.out.println("boardDEPT" + boardCatCd );
+		}
 		// map에 담아 변수로 넘기게
 		Map<String, Object> paramMap = new HashMap<String,Object>();
 		paramMap.put("beginRow", beginRow);
 		paramMap.put("rowPerPage", rowPerPage);
-		log.debug("\u001B[31m"+ paramMap + "< paramMap" + "\u001B[0m");	
+		paramMap.put("boardCatCd", boardCatCd);
+		log.debug("\u001B[41m"+ paramMap + "< paramMap" + "\u001B[0m");	
 		
-		// 공지사항 리스트 조회
-		List<Board> selectNotice = boardMapper.selectNotice(paramMap);
-		log.debug("\u001B[31m"+ selectNotice + "< selectNotice" + "\u001B[0m");	
+		// 각 리스트 조회
+		List<Board> selectBoard = boardMapper.selectBoard(paramMap);
+		log.debug("\u001B[41m"+ selectBoard + "< selectBoard" + "\u001B[0m");	
 		
-		// 페이징을 위한 공지사항 전체 개수 
-		int boardCount = boardMapper.selectNoticeCount();
 		
 		// ================ 페이지 =================
+		// 페이징을 위한 게시판 별 게시물 전체 개수 
+		int boardCount = boardMapper.selectBoardCount(boardCatCd);
+		
 		// 마지막행
 		int endRow = beginRow + (rowPerPage -1);
 		if(endRow > boardCount) {
@@ -70,7 +86,7 @@ public class BoardService {
 		
 		// 맵에 담아 넘기기
 		Map<String, Object> resultMap = new HashMap<String,Object>();
-		resultMap.put("selectNotice", selectNotice);
+		resultMap.put("selectBoard", selectBoard);
 		resultMap.put("currentPage", currentPage);
 		resultMap.put("lastPage", lastPage);
 		resultMap.put("endRow", endRow);
